5f224ec8a52acaddd75b3f23b9e849e29b7ff145
Fabrice-64 <fab.jaouen@gmail.com>
2021-02-06 09:21:55 +0100

    Update WSGI configuration with app name

--- papounet_super_diet/wsgi.py
+++ papounet_super_diet/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'papounet_diet.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'papounet_super_diet.settings')
 
 application = get_wsgi_application()
