f6563d241b4588ddb304411d48534216df619abc
Fabrice-64 <fab.jaouen@gmail.com>
2021-02-28 10:25:49 +0100

    Remove useless e-mail test.

--- customer/tests/tests.py
+++ customer/tests/tests.py
@@ -78,16 +78,3 @@ class ChangePasswordTest(TestCase):
                                                                    'new_password2': 'user3'})
         self.assertTemplateUsed(response, 'customer/password_change.html')
 
-
-class EmailTest(TestCase):
-    def test_send_email(self):
-        # Send message.
-        mail.send_mail(
-            'Subject here', 'password_change',
-            'contact@papounet-super-diet.fr', ['to@example.com'],
-            fail_silently=False,
-        )
-        # Test that one message has been sent.
-        self.assertEqual(len(mail.outbox), 1)
-        # Verify that the subject of the first message is correct.
-        self.assertEqual(mail.outbox[0].subject, 'Subject here')
